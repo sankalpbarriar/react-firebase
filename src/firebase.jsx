780cc2757190661483fc5a954ce120e7ed5e1fbd
sankalpbarriar <sankalpbarriar10@gmail.com>
2024-03-09 18:16:38 +0530

    Inital commit

---
+++ src/firebase.jsx
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBl4Mc7cX3rUZJ7XCB4oKeYZZv95Fat6R8",
+  authDomain: "apps-b958a.firebaseapp.com",
+  projectId: "apps-b958a",
+  storageBucket: "apps-b958a.appspot.com",
+  messagingSenderId: "845710736610",
+  appId: "1:845710736610:web:e6a3248bbe8e2e8bd9b2bb",
+  databaseURL:"https://console.firebase.google.com/project/apps-b958a/database/apps-b958a-default-rtdb/data/~2F",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
